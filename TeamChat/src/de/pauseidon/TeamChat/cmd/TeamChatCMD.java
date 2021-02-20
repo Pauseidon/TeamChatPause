1b7ab7e0efbb173ba3f09f6df4a09af35ab8ab0b
Pauseidon <79382162+Pauseidon@users.noreply.github.com>
2021-02-20 23:18:24 +0100

    Add files via upload

---
+++ TeamChat/src/de/pauseidon/TeamChat/cmd/TeamChatCMD.java
@@ -0,0 +1,48 @@
+/**
+Copyright Paul-Theodor.de to Present
+All rights reserved.
+*/
+package de.pauseidon.TeamChat.cmd;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import de.pauseidon.TeamChat.main;
+
+public class TeamChatCMD implements CommandExecutor {
+
+	// I SAID FUCK OFF AND DONT STEAL THE FUCKING CODE JUST DOWNLOAD IT FROM MY ODYSEE!
+	
+	String Prefix = main.getPlugin().getConfig().getString("Prefix"); 
+	String Permission = main.getPlugin().getConfig().getString("Permission"); 
+	String color = main.getPlugin().getConfig().getString("NameColor");
+	String mcolor = main.getPlugin().getConfig().getString("MessageColor"); 
+	String nomessage = main.getPlugin().getConfig().getString("NoMessageAfterCommand"); 
+	String alias = main.getPlugin().getConfig().getString("CommandAlias"); 
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String lable, String[] args) {
+		if (command.getName().equalsIgnoreCase("tc") || command.getName().equalsIgnoreCase("teamchat") || command.getName().equalsIgnoreCase(alias)) {
+			if (sender instanceof Player) {
+				Player p = (Player) sender;
+				if (p.hasPermission(Permission)) {
+					if (args.length == 0) {
+						p.sendMessage(Prefix + " " + nomessage);
+					}
+					if (args.length != 0) {
+						for (Player player : Bukkit.getOnlinePlayers()) {
+								player.sendMessage(Prefix + " " + color + p.getDisplayName() + mcolor + args[0].toString());
+						}
+					}
+				} else
+					p.sendMessage(Prefix  + " " +  Permission);
+			}
+
+		}
+		return false;
+	}
+}
+
---
+++ TeamChat/src/de/pauseidon/TeamChat/main.java
@@ -0,0 +1,103 @@
+/**
+Copyright Paul-Theodor.de to Present
+All rights reserved.
+*/
+package de.pauseidon.TeamChat;
+
+import java.nio.file.Paths;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.pauseidon.TeamChat.cmd.PluginInfoCMD;
+import de.pauseidon.TeamChat.cmd.TeamChatCMD;
+
+public class main extends JavaPlugin {
+
+	private static main plugin;
+
+// Startup	
+	public void onLoad() {
+
+		saveDefaultConfig();
+
+	}
+
+	public void onEnable() {
+		plugin.saveDefaultConfig();
+		loadconfig();
+		sendStartMessage();
+		registerEvents();
+		getCommands();
+
+
+	}
+
+// Shutdown
+	public void onDisable() {
+		sendStopMessage();
+	}
+
+// Plugin Instance
+	public main() {
+		if (main.plugin != null) {
+			throw new Error("Plugin already initialized!");
+		}
+		main.plugin = this; // A plugin's constructor should only be called once
+	}
+
+	public static main getPlugin() {
+		return plugin;
+	}
+
+// System Prints    
+	public void sendStartMessage() {
+// Press Insert/Einfg to keep the format
+		System.out.println("------------------------------");
+		System.out.println("|            [Team]          |");
+		System.out.println("|       Made by Pauseidon    |");
+		System.out.println("|          Activated         |");
+		System.out.println("------------------------------");
+	}
+
+	public void sendStopMessage() {
+// Press Insert/Einfg to keep the format
+		System.out.println("------------------------------");
+		System.out.println("|            [Team]          |");
+		System.out.println("|       Made by Pauseidon    |");
+		System.out.println("|          Deactivated       |");
+		System.out.println("------------------------------");
+	}
+
+// Event registration    
+	private void registerEvents() {
+//		Bukkit.getServer().getPluginManager().registerEvents(new ClassName(), this);
+//		Bukkit.getServer().getPluginManager().registerEvents(new ClassName(), this);
+//		Bukkit.getServer().getPluginManager().registerEvents(new ClassName(), this);
+//		Bukkit.getServer().getPluginManager().registerEvents(new ClassName(), this);
+//		Bukkit.getServer().getPluginManager().registerEvents(new ClassName(), this);
+//		Bukkit.getServer().getPluginManager().registerEvents(new ClassName(), this);
+//		Bukkit.getServer().getPluginManager().registerEvents(new ClassName(), this);
+//		Bukkit.getServer().getPluginManager().registerEvents(new ClassName(), this);
+	}
+
+// Command registration
+	private void getCommands() {
+		getCommand("plugininfo").setExecutor((CommandExecutor) new PluginInfoCMD());
+		getCommand("tc").setExecutor((CommandExecutor)new TeamChatCMD());
+//		getCommand("COMMAND").setExecutor((CommandExecutor)new ClassName());
+//		getCommand("COMMAND").setExecutor((CommandExecutor)new ClassName());
+//		getCommand("COMMAND").setExecutor((CommandExecutor)new ClassName());
+//		getCommand("COMMAND").setExecutor((CommandExecutor)new ClassName());
+//		getCommand("COMMAND").setExecutor((CommandExecutor)new ClassName()),
+	}
+
+
+	public void loadconfig() {
+		if (!Paths.get("plugins", new String[] { "TeamChat", "config.yml" }).toFile().exists()) {
+			getConfig().options().copyDefaults(true);
+			saveConfig();
+		}
+
+	}
+}
